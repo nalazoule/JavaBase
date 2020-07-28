8cc87bf269fe21109f823c43b9cedf7747389800
jiangxuewei <15176133160@163.com>
2020-07-29 01:53:51 +0800

    1

--- Demo/src/cn/demo/Class1.java
+++ Demo/src/cn/demo/Class1.java
@@ -5,5 +5,6 @@ package cn.demo;
 public class Class1 {
 	public static void main(String[] args) {
 		System.out.println("Class1");
+		// 2222
 	}
 }
--- Demo/src/cn/demo/Class2.java
+++ Demo/src/cn/demo/Class2.java
@@ -5,5 +5,6 @@ package cn.demo;
 public class Class2 {
 	public static void main(String[] args) {
 		System.out.println("bbbbb");
+		// 2222
 	}
 }
