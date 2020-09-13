e8d66a20f93424130d0d3600089793c34b98aed3
nalazoule <68148618+nalazoule@users.noreply.github.com>
2020-09-14 01:32:18 +0800

    Update Class2.java

--- Demo/src/cn/demo/Class2.java
+++ Demo/src/cn/demo/Class2.java
@@ -5,6 +5,12 @@ package cn.demo;
 public class Class2 {
 	public static void main(String[] args) {
 		//2222
+		//3333
 		System.out.println("bbbbb");
+		//44444
 	}
+}
+class student{
+	private String id;
+	private String name;
 }
