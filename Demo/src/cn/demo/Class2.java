00e0fc63d7f17007c83008a7ec4be131b6ca04f2
jiangxuewei <15176133160@163.com>
2020-09-15 00:48:27 +0800

    修改class2

--- Demo/src/cn/demo/Class2.java
+++ Demo/src/cn/demo/Class2.java
@@ -1,20 +1,24 @@
 package cn.demo;
 /**
  * Class2222222222
+ * Class22----22
  * 
  * @author 15176
  *
  */
 public class Class2 {
 	public static void main(String[] args) {
+		System.out.println("这是一个测试Class2---------");
+		System.out.println("22222");
 		System.out.println("这是一个测试Class2");
 		System.out.println("bbbbb");
 	}
 }
 
 class student {
-	// student的name
+	// student的name----
+	// ----
 	private String name;
-	// student的id
+	// student的id----
 	private String id;
 }
