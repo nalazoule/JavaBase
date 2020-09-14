c5476d0362c0f0c3dde3800d595adc89c1110712
jiangxuewei <15176133160@163.com>
2020-09-15 01:07:06 +0800

    ee

--- Demo/src/cn/demo/Class2.java
+++ Demo/src/cn/demo/Class2.java
@@ -1,30 +1,19 @@
 package cn.demo;
+
 /**
- * Class2222222222
- * Class
- * Class22----22
  * @author 15176
  *
  */
 public class Class2 {
 	public static void main(String[] args) {
-		System.out.println("这是一个测试Class2---------");
-		System.out.println("22222");
-		System.out.println("Class2");
-		System.out.println("这是一个测试Class2");
+		// few
 		System.out.println("bbbbb");
 	}
 }
 
 class student {
-	// student的name----
-	// student的name000
-	// ----
-	// student的name000
+	// 212321
 	private String name;
-	// student的id----
-	// student的id000
-	// student的id000
-	// student的id000
+	// fewa
 	private String id;
 }
