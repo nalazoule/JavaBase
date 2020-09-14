09c57510f86440ba592d7d4530fd056f60d58502
jiangxuewei <15176133160@163.com>
2020-09-14 11:46:05 +0800

    修改

--- Demo/src/cn/demo/Class2.java
+++ Demo/src/cn/demo/Class2.java
@@ -1,14 +1,14 @@
 package cn.demo;
 
 /**
- * Class211111111111111
+ * Class2222222222
  * 
  * @author 15176
  *
  */
 public class Class2 {
 	public static void main(String[] args) {
-		System.out.println("bbbbb");//111111111
+		System.out.println("bbbbb");// 2222222
 	}
 }
 
