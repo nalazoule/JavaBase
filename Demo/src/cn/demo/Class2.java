e61966ba7781edeb251f45b95e95e63b675f34bc
nalazoule <68148618+nalazoule@users.noreply.github.com>
2020-09-15 00:15:55 +0800

    Update Class2.java

--- Demo/src/cn/demo/Class2.java
+++ Demo/src/cn/demo/Class2.java
@@ -1,20 +1,20 @@
 package cn.demo;
 /**
- * Class2222222222
+ * Class
  * 
  * @author 15176
  *
  */
 public class Class2 {
 	public static void main(String[] args) {
-		System.out.println("这是一个测试Class2");
+		System.out.println("Class2");
 		System.out.println("bbbbb");
 	}
 }
 
 class student {
-	// student的name
+	// student的name000
 	private String name;
-	// student的id
+	// student的id000
 	private String id;
 }
